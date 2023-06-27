f5a094b659d45e103cc38837cbbdaac89cb0da39
Igor Martynchik <sepsetto@gmail.com>
2023-06-27 10:09:13 +0300

    Homework seminar 5

---
+++ 5Task36/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int arrayLength = 4;
+        int[] numbers = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++)
+        {
+            numbers[i] = random.Next(1, 100);
+        }
+        int sum = 0;
+        for (int i = 1; i < arrayLength; i += 2)
+        {
+            sum += numbers[i];
+        }
+        Console.WriteLine("Массив случайных чисел:");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("Сумма элементов на нечетных позициях: " + sum);
+    }
+}
---
+++ 5Task38/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] numbers = { 3.14, 2.71, 1.618, 0.577, 2.718 };
+        double maxNumber = numbers[0];
+        double minNumber = numbers[0];
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] > maxNumber)
+            {
+                maxNumber = numbers[i];
+            }
+            if (numbers[i] < minNumber)
+            {
+                minNumber = numbers[i];
+            }
+        }
+        double difference = maxNumber - minNumber;
+        Console.WriteLine("Массив вещественных чисел:");
+        foreach (double number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("Разница между максимальным и минимальным элементами: " + difference);
+    }
+}
