5d222e189f94d469554ce80c6ace3dc9a8093bd3
Igor Martynchik <sepsetto@gmail.com>
2023-06-27 09:55:29 +0300

    Homework seminar 5

---
+++ 4Task34/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int arrayLength = 4;
+        int[] numbers = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++)
+        {
+            numbers[i] = random.Next(100, 1000);
+        }
+        int evenCount = 0;
+        for (int i = 0; i < arrayLength; i++)
+        {
+            if (numbers[i] % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+        Console.WriteLine("Массив случайных трехзначных чисел:");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("Количество четных чисел: " + evenCount);
+    }
+}
